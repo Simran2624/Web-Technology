55edb9a66132bb12dde77dac4aef5fc422971468
Laxmi Wandare <aishvaryarv@gmail.com>
2024-03-20 00:03:53 +0530

    Added random number Array generator

---
+++ index.js
@@ -0,0 +1,25 @@
+function arraySizeChange(n){
+    let array=[];
+    let text="Array :";
+    for (let i = 0; i < n-1; i++) {
+        array.push(Math.floor(Math.random() * (n+20 - 1 + 1)) + 1);
+        text+= ` ${array[i]}`
+        text+=','
+    }
+    array.push(Math.floor(Math.random() * (n+20 - 1 + 1)) + 1);
+    text+= ` ${array[n-1]}`
+
+    console.log(array);
+    
+    arrayHeading.textContent = text;
+}
+// Get reference to the range input element
+const arrayHeading = document.getElementById('array');
+const arrayRangeInput = document.getElementById('arrayRange');
+
+
+// Add event listener for the 'input' event
+arrayRangeInput.addEventListener('input', function() {
+  // Call the arraySizeChange function passing the new value
+  arraySizeChange(arrayRangeInput.value);
+});
